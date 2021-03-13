927b0228c6c7782d5ee3902309fa945bbaa0c756
sid-r-singh <sid-r-singh@users.noreply.github.com>
2021-03-13 10:43:38 +0000

    Deployed updated by GH-action

--- assets/js/d30d3275.f908ab62.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[57],{157:function(a){a.exports=JSON.parse('{"allTagsPath":"/documents/blog/tags","slug":"agile","name":"agile","count":1,"permalink":"/documents/blog/tags/agile"}')}}]);
