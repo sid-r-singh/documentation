76d3b3e8ac0452e2708ebfa9282eb0eb6471b599
Sidharth R <68782815+sid-r-singh@users.noreply.github.com>
2021-03-13 14:59:13 +0530

    Create siteConfig.js

---
+++ siteConfig.js
@@ -0,0 +1,8 @@
+const siteConfig = {
+  ...
+  url: 'https://sidrana.dev', // Your website URL
+  baseUrl: '/documentation/',
+  projectName: 'documentation',
+  organizationName: 'sid-r-singh'
+  ...
+}
